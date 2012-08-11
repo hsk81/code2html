093cf16603688dee0fc311d39bf900dd46e2d455
Hasan Karahan <hasan.karahan81@gmail.com>
2012-08-11 11:26:10 +0200

    run on any host port 5011

--- code2html.py
+++ code2html.py
@@ -52,4 +52,4 @@ def security_check (ext):
 if __name__ == "__main__":
 ###############################################################################
 
-    app.run (debug=False)
+    app.run (host='0.0.0.0', port=5001, debug=False)
